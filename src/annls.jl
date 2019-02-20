151e7300b7cdbe8128ae3caddf1f17a850def9fd
Benjamin Antin <bantin@stanford.edu>
2019-02-19 18:39:07 -0800

    ANNLS: Add parallel update option

--- src/annls.jl
+++ src/annls.jl
@@ -27,8 +27,11 @@ function update(data, W, H, meta, options)
     # W update
     _update_W!(data, W, H)
 
-    # H update
-    _update_H!(data, W, H)
+    if get(options, "mode", nothing) == "parallel"
+        _update_H_parallel!(data, W, H)
+    else
+        _update_H!(data, W, H)
+    end
 
     meta.resids = tensor_conv(W, H) - data
     return norm(meta.resids) / meta.data_norm, meta
@@ -51,23 +54,54 @@ function _update_W!(data, W, H)
     W[:,:,:] = fold_W(t(W_unfold), L, N, K)
 end
 
+"""
+Perform H update updating every L colums of H simultaneously.
+"""
+function _update_H_parallel!(data, W, H)
+    K, T = size(H)
+    L, N, K = size(W)
+
+    # Update every L columns of H
+    ind_set_total = []
+    for start in 1:L
+        ind_set = start:L:T
+        B = zeros(N,length(ind_set))
+        for (i, t) in enumerate(ind_set)
+            B[:,i] = form_b_vec(data, W, H, t)
+        end
+        # Solve L NNLS problems
+        H[:,ind_set] = NonNegLeastSquares.nonneg_lsq(W[1,:,:], B, alg=:pivot)
+    end
+end
+
+"""
+Perform H update a single column at a time
+"""
 function _update_H!(data, W, H)
     K, T = size(H)
     L, N, K = size(W)
 
-    # Update H column-by-column
     for t in 1:T
-        v = zeros(N,1)
-        for l = 1:L-1
-            h_idx = t-l
-            if h_idx <= 0
-                continue
-            end
-            v += W[l+1,:,:] * H[:,h_idx]
-        end
-        b = data[:,t] - v
+        b = form_b_vec(data, W, H, t)
         H[:,t] = NonNegLeastSquares.nonneg_lsq(W[1,:,:], b, alg=:pivot)
     end
+end
+
+"""
+Forms the vector b for the nonnegative least squres objective
+||Ax - b|| required to update column t of H
+"""
+function form_b_vec(data, W, H, t)
+    L, N, K = size(W)
+    v = zeros(N,1)
+    for l = 1:L-1
+        h_idx = t-l
+        if h_idx <= 0
+            continue
+        end
+        v += W[l+1,:,:] * H[:,h_idx]
+    end
+    return data[:,t] - v
 end
 
 """
