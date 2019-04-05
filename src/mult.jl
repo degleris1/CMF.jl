1baa3de8e22e56da7aa23bfa92fcdddd310be7bf
Benjamin Antin <bantin@stanford.edu>
2019-04-05 16:42:05 -0700

    Add Regularizers to Mult
    Regularizers are passed as kwargs to the
    fit_cnmf function. Currently, passing regularizers to algorithms other
    than mult will have no effect.

--- src/mult.jl
+++ src/mult.jl
@@ -6,15 +6,22 @@ include("./common.jl")
 
 
 function update!(data, W, H, meta; kwargs...)
+
+    # unpack regularization args
+    l1_W = get(kwargs, :l1_W, 0)
+    l1_H = get(kwargs, :l1_H, 0)
+    l2_W = get(kwargs, :l2_W, 0)
+    l2_H = get(kwargs, :l2_H, 0)
+
     if (meta == nothing)
         meta = MultMeta(data, W, H)
     end
     
     num_W, denom_W = _compute_mult_W(data, W, H)
-    W .*= num_W ./ (denom_W .+ EPSILON)
+    W .*= num_W ./ (denom_W .+ l1_W .+ 2 .* l2_W .* W .+ EPSILON)
 
     num_H, denom_H = _compute_mult_H(data, W, H)
-    H .*= num_H ./ (denom_H .+ EPSILON)
+    H .*= num_H ./ (denom_H .+ l1_H .+ 2 .* l2_H .* H .+ EPSILON)
 
     # Cache resids
     meta.resids = compute_resids(data, W, H)
