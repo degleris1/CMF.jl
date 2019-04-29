485d2841da3793df6cc0119b04b4b71f8e4fda5a
Benjamin Antin <bantin@stanford.edu>
2019-04-29 02:55:45 -0700

    Add imshow formatting (#42)
    Add ability to add border to a PyPlot axis. Useful for generating figures when showing the W components.

--- src/visualize.jl
+++ src/visualize.jl
@@ -31,15 +31,33 @@ function plot_Ws(
 
     fig, ax = plt.subplots(1, num_components(r))
     for (k, a) in enumerate(ax)
+        format_imshow_axis(a)
         a.imshow(transpose(W[:, idx, k]), aspect="auto")
     end
 
     if !(trueW === nothing)
         fig2, ax2 = plt.subplots(1, num_components(r))
         for (k, a) in enumerate(ax2)
+            format_imshow_axis(a)
             a.imshow(transpose(trueW[:, idx, k]), aspect="auto")
         end
     end
 
     return fig, ax
+end
+
+function format_imshow_axis(
+                            ax; 
+                            border_width=1, 
+                            border_color="black",
+                            remove_ticks=true
+                            )
+    if remove_ticks
+        ax.set_yticks([])
+        ax.set_xticks([])
+    end
+    for side in ["left", "right", "bottom", "top"]
+        ax.spines[side].set_linewidth(border_width)
+        ax.spines[side].set_color(border_color)
+    end
 end
