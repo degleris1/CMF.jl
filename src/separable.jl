a4cfd2cb970e8fe12560552ec9f521e95eea561d
Anthony Degleris <degleris1@gmail.com>
2019-05-06 15:16:18 +0200

    Noiseless algorithm now works provably given additional condition

--- src/separable.jl
+++ src/separable.jl
@@ -6,7 +6,7 @@ using PyPlot; plt = PyPlot
 include("./common.jl")
 
 
-function generate_separable_data(;N=100, T=250, K=3, L=8, H_sparsity=0.8, W_sparsity=0.75)
+function generate_separable_data(;N=23, T=53, K=3, L=5, H_sparsity=0.8, W_sparsity=0.75)
     # Generate W
     trueW = 10 * rand(L, N, K) .* (rand(L, N, K) .> W_sparsity)
     
@@ -16,10 +16,6 @@ function generate_separable_data(;N=100, T=250, K=3, L=8, H_sparsity=0.8, W_spar
         trueH[k, (k-1)*L+1] = 1
     end
     trueH[:, K*L+1:end] = rand(K, T-K*L) .* (rand(K, T-K*L) .> H_sparsity)
-    # Make entire
-    trueH[:, T-L:end] .= 0
-    # Columns should sum to 1
-    trueH = trueH * inv(diagnorm(trueH, 1))
     
     # Generate data
     X = tensor_conv(trueW, trueH)
@@ -34,11 +30,6 @@ function fit_conv_separable(data, K, L)
 
     # Step 2: compute unconstrained H (NMF)
     Ho = nonneg_lsq(Wo, data, alg=:pivot, variant=:comb)
-    
-    # DEBUG
-    println("NNLS loss: ", norm(Wo * Ho - data))
-    println(vertices)
-    # -----
     
     # Step 3: group and sort rows of H to produce convolutive H
     H, groups = shift_cluster(Ho, K, L)
@@ -56,28 +47,14 @@ end
 function shift_cluster(Ho, K, L)
     R, T = size(Ho)
 
-    # Step 1: compute similarity matrix
-    simat = zeros(R, R)
+    # Step 1: compute distance matrix
     dmat = zeros(R, R)
     for r = 1:R
         for p = r:R
-            simat[r, p] = compute_sim(Ho[r, :], Ho[p, :], L)
-            simat[p, r] = simat[r, p]
-
             dmat[r, p] = shift_dist(Ho[r, :], Ho[p, :], L)
             dmat[p, r] = dmat[r, p]
         end
     end
-
-    plt.figure()
-    plt.title("Similarity")
-    plt.imshow(simat)
-    plt.colorbar()
-
-    plt.figure()
-    plt.title("Distance")
-    plt.imshow(dmat)
-    plt.colorbar()
     
     # Step 2: compute groups
     group = [[] for k in 1:K]
@@ -88,8 +65,8 @@ function shift_cluster(Ho, K, L)
 
         while (length(group[k]) < L)
             # Add the element closest to the group
-            sims = sum(simat[group[k], ungrouped], dims=1)
-            _, i = findmax(sims)
+            dists = sum(dmat[group[k], ungrouped], dims=1)
+            _, i = findmin(dists)
             i = i[2]
 
             push!(group[k], ungrouped[i])
@@ -121,35 +98,28 @@ function shift_dist(h1, h2, L; dist=euclid_dist)
 end
 
 
-""" Shift similarity between h1 and h2. """
-function compute_sim(h1, h2, L)
-    T = length(h1)
-    
-    best_sim = cosine(h1, h2)
-    for l = 1:L-1
-        best_sim = max(best_sim,
-                       cosine(h1[1:T-l], h2[1+l:T]),  # Shift h1 right
-                       cosine(h1[1+l:T], h2[1:T-l]))
-    end
-    
-    return best_sim
-end
-
-
 """ Successive projection algorithm. """
-function SPA(data, n)
-    DX = diagnorm(data, 1)
+function SPA(data, K)
+    coldata = colnorms(data)
+    DX = diagscale(colnorms(data, 1))
     X = data * inv(DX)
     
     vertices = []
-    R = X
+    resid = X
     
-    for r = 1:n
-        _, j = findmax(colnorms(R))
+    for r = 1:K
+        _, jset = findsetmax(colnorms(resid))
+
+        if (length(jset) == 1)
+            j = jset[1]
+        else  # Break ties
+            _, k = findmax(coldata[jset])
+            j = jset[k]
+        end
         push!(vertices, j)
 
-        w = R[:, j]
-        R = (I - (w * w' / norm(w)^2)) * R
+        w = resid[:, j]
+        resid = (I - (w * w' / norm(w)^2)) * resid
     end
     
     return data[:, sort(vertices)], sort(vertices)
@@ -161,18 +131,34 @@ colnorms(A, p=2) = [norm(A[:, t], p) for t = 1:size(A, 2)]
 
 
 """ Normalization matrix. """
-function diagnorm(A, p=2)
-    c = colnorms(A, p)
+function diagscale(c)
     return diagm(0 => c + ones(size(c)) .* (c .< eps()))
 end
 
 
-""" Compute the cosine of the angle between two vectors. """
-cosine(a, b) = a'b / (norm(a) * norm(b))
-
-
 """ Compute the Euclidean distance between a and b. """
 euclid_dist(a, b) = norm(a - b)
+
+
+""" Return set of maximal values. """
+function findsetmax(x; thresh=eps()^(1/2))
+    n = length(x)
+
+    maxval = x[1]
+    set = [1]
+
+    for i in 2:n
+        if (x[i] > maxval + thresh)
+            maxval = x[i]
+            set = [i]
+        elseif (x[i] > maxval - thresh)
+            push!(set, i)
+        end
+    end
+            
+    return maxval, set
+end
+
 
 
 """
@@ -183,7 +169,7 @@ data, tW, tH, (N, T, K, L) = generate_separable_data()
 W, H = fit_conv_separable(data, K, L)
 
 
-"""
+
 plt.figure()
 plt.imshow(H, aspect="auto")
 plt.title("Fit")
@@ -193,4 +179,3 @@ plt.figure()
 plt.imshow(tH, aspect="auto")
 plt.title("Truth")
 plt.show()
-"""
