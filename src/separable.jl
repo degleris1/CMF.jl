8b4cece614579b8f84b53ba6dd7379d22a9e03cb
Anthony Degleris <degleris1@gmail.com>
2019-05-21 18:43:25 +0200

    Moved unstructured separable dataset to its own file

--- src/separable.jl
+++ src/separable.jl
@@ -1,7 +1,7 @@
 module Separable
 
 export fit_conv_separable
-export gen_sep_data, is_separable
+export is_separable
 export cos_score, permute_factors, row_normalize
 
 using NonNegLeastSquares
@@ -308,50 +308,6 @@ function permute_factors(trueH, estH)
     permset = collect(permutations(1:size(trueH, 1)))     
     resid, i = findmax([cos_score(estH[p, :], trueH) for p in permset])
     return permset[i]
-end
-
-
-"""
-DATA GENERATION
-"""
-
-
-""" Generate separable data. """
-function gen_sep_data(N, T, K, L; H_sparsity=0.75)
-
-    # Generate W
-    W = 0.5 .+ rand(L, N, K) 
-    
-    # Generate H
-    H = rand(K, T) .* (rand(K, T) .> H_sparsity)
-
-    # Add separable factors
-    if (T < 3*K*L)
-        println("T too small")
-        return nothing
-    end
-
-    hL = floor(Int, L/2)
-    times = collect(1:T-L)
-    free = (times .!= 0)
-    
-    for k = 1:K
-        for (down, up) in [(-L, hL), (-hL, L)]  # Left and ride side of sequence 
-            t = rand(times[free])
-            t1 = max(1, t+down)
-            t2 = min(T, t+up)
-
-            H[:, t1:t2] .= 0
-            H[k, t] = 0.5 + rand()
-
-            free[t1:min(t2,T-L)] .= false
-        end
-    end
-    
-    # Generate data
-    X = tensor_conv(W, H)
-    
-    return X, W, H
 end
 
 
