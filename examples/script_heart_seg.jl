925749ef8120f1efccebdff9108f307793895169
degleris1 <degleris1@gmail.com>
2020-03-21 14:41:40 -0400

    Messing with fft notebook

--- examples/script_heart_seg.jl
+++ examples/script_heart_seg.jl
@@ -1,11 +1,8 @@
 using MAT
 import PyPlot; plt = PyPlot
 
-
-matdict = matread("/home/asd/data/heart/results/train_1.mat")
-signal = matdict["signal"]
-println(size(signal))
-
-plt.figure()
-plt.plot(signal[1, 1:500])
-plt.show()
+function runscript()
+    path = "/home/asd/data/heart/"
+    md_groups = matread(path * "groups.mat")
+    groups = md_groups["groups"]
+end
