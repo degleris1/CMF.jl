bb2a4800004170a2358a55a9128c1800e7cd2382
degleris1 <degleris1@gmail.com>
2020-04-06 13:32:41 -0400

    Relabeled some directories

--- examples/script_heart_seg.jl
+++
@@ -1,8 +0,0 @@
-using MAT
-import PyPlot; plt = PyPlot
-
-function runscript()
-    path = "/home/asd/data/heart/"
-    md_groups = matread(path * "groups.mat")
-    groups = md_groups["groups"]
-end
